4eeb305d21c97d5a156b49adf6b1098f01282154
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-02 14:58:09 -0400

    add cartPage

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,3 +1,25 @@
-const CartPage = () => {};
+import { useEffect } from "react";
+import { Link } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { addToCart } from "../actions/cartActions";
+import { useLocation, useParams } from "react-router-dom";
+
+const CartPage = () => {
+  const id = useParams();
+
+  const location = useLocation().search.split("=")[1];
+  const quantity = location ? Number(location) : 1;
+  console.log("qty", quantity);
+  console.log("productId", id);
+};
 
 export default CartPage;
