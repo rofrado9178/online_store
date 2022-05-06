30ed9886e1380a9d33f9d3be771c5cad782b3d05
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-06 15:11:57 -0400

    add api call for all users

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,12 +1,15 @@
 from os import access
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .models import *
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 
 
 # Create your views here.
@@ -55,7 +58,15 @@ def getProduct(request, id):
   return Response(serializer.data)
 
 @api_view(["GET"])
-def getUser(request):
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
   user = request.user
   serializer = UserSerializer(user,many=False)
+  return Response(serializer.data)
+
+@api_view(["GET"])
+# @permission_classes([IsAdminUser])
+def getUsers(request):
+  users = User.objects.all()
+  serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
