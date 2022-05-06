a0ac1324cabfdb8016257e3d2f119a55dec22eaf
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-06 15:14:44 -0400

    add admin authentication to see all users

--- backend/base/views.py
+++ backend/base/views.py
@@ -65,7 +65,7 @@ def getUserProfile(request):
   return Response(serializer.data)
 
 @api_view(["GET"])
-# @permission_classes([IsAdminUser])
+@permission_classes([IsAdminUser])
 def getUsers(request):
   users = User.objects.all()
   serializer = UserSerializer(users, many=True)
