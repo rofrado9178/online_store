bc3dc9a5c514c9061dad088398be93615b017476
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-06 15:44:46 -0400

    add an exception if user with email already exsists

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 
 urlpatterns = [
-  path("users/login/", views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path("users/login", views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
   path("users/profile", views.getUserProfile, name="user-profile"),
   path("users", views.getUsers, name="users"),
+  path("users/register", views.registerUser , name="register"),
   path("", views.getRoutes, name="routes"),
   path("products", views.getProducts, name="products"),
   path("products/<str:id>", views.getProduct, name="product"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,23 @@
 from os import access
 from django.shortcuts import render
 from django.http import JsonResponse
+#for user authentication and admin and view
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+#response back data
 from rest_framework.response import Response
+#import all models
 from .models import *
+#send back JSON data
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+#jwt token auth
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+
 
 
 
@@ -70,3 +80,24 @@ def getUsers(request):
   users = User.objects.all()
   serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
+
+@api_view(["POST"])
+def registerUser(request):
+
+  data = request.data
+  
+  try:
+    user = User.objects.create(
+      first_name = data["first_name"],
+      last_name = data["last_name"],
+      username = data["email"],
+      email = data["email"],
+      password = make_password(data["password"])
+    )
+
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+  
+  except:
+    message = {"User with this email already exists"}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
