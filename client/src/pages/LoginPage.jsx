6e88070dd75469f1d26bc0d5fbd6f87a777d2075
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-05-10 15:18:46 -0400

    add user login page and login logic

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -5,12 +5,48 @@ import { useDispatch, useSelector } from "react-redux";
 import Loading from "../components/Loading";
 import Message from "../components/Message";
 import { loginUser } from "../actions/userActions";
+import FormContainer from "../components/FormContainer";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  return <h1>Hello</h1>;
+  const dispatch = useDispatch();
+
+  const login = (event) => {
+    event.preventDefault();
+    console.log("submited");
+    dispatch(loginUser(email, password));
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={login}>
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Email address"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default Login;
