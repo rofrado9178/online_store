7271f30f255ce10b0a2b007664d90bfb5780ad4b
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-06-10 13:42:22 -0400

    add conditional render to fix component error

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -33,6 +33,7 @@ const ProfilePage = () => {
     // setPassword("");
     // setConfirmPassword("");
     // setEmail("");
+    dispatch(userProfile("profile"));
   };
 
   useEffect(() => {
@@ -43,77 +44,82 @@ const ProfilePage = () => {
     if (!profile || !profile.name || success) {
       dispatch({ type: UPDATE_PROFILE_RESET });
       dispatch(userProfile("profile"));
-      return;
     }
   }, [user, dispatch, navigate, success, profile]);
 
   return (
     <Row>
-      <Col md={3}>
-        <h2>User Profile</h2>
-        {message && <Message variant="danger">{message}</Message>}
-        {/* {error && <Message variant="danger">{error.message}</Message>} */}
-        {loading && <Loading />}
-        <Form onSubmit={updateUserProfile}>
-          <Form.Label>Full Name</Form.Label>
-          <p>{profile.name}</p>
-          <Form.Label>Email</Form.Label>
-          <p>{profile.email}</p>
-          <Form.Group controlId="first_name">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control
-              type="first_name"
-              placeholder="First Name"
-              value={first_name}
-              onChange={(event) => setFirstName(event.target.value)}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group controlId="last_name">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              type="last_name"
-              placeholder="Last Name"
-              value={last_name}
-              onChange={(event) => setLastName(event.target.value)}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group controlId="last_name">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            ></Form.Control>
-          </Form.Group>
+      {!profile ? (
+        <Loading />
+      ) : (
+        <Row>
+          <Col md={3}>
+            <h2>User Profile</h2>
+            {message && <Message variant="danger">{message}</Message>}
+            {/* {error && <Message variant="danger">{error.message}</Message>} */}
+            {loading && <Loading />}
+            <Form onSubmit={updateUserProfile}>
+              <Form.Label>Full Name</Form.Label>
+              <p>{profile.name}</p>
+              <Form.Label>Email</Form.Label>
+              <p>{profile.email}</p>
+              <Form.Group controlId="first_name">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="first_name"
+                  placeholder="First Name"
+                  value={first_name}
+                  onChange={(event) => setFirstName(event.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="last_name">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="last_name"
+                  placeholder="Last Name"
+                  value={last_name}
+                  onChange={(event) => setLastName(event.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="last_name">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group controlId="password">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              value={confirmPassword}
-              onChange={(event) => setConfirmPassword(event.target.value)}
-            ></Form.Control>
-          </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter password"
+                  value={confirmPassword}
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                ></Form.Control>
+              </Form.Group>
 
-          <Button type="submit" variant="dark" className="rounded my-4">
-            Update Profile
-          </Button>
-        </Form>
-      </Col>
-      <Col md={9}>
-        <h2>My Orders</h2>
-      </Col>
+              <Button type="submit" variant="dark" className="rounded my-4">
+                Update Profile
+              </Button>
+            </Form>
+          </Col>
+          <Col md={9}>
+            <h2>My Orders</h2>
+          </Col>{" "}
+        </Row>
+      )}
     </Row>
   );
 };
