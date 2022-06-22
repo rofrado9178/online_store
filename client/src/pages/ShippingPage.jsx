ab3a6c77b3131aa89bcf73b26218e2d67a8e44dc
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-06-22 15:35:31 -0400

    add checkout steps component

--- client/src/pages/ShippingPage.jsx
+++ client/src/pages/ShippingPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 import { useNavigate } from "react-router-dom";
 
@@ -35,6 +36,7 @@ const ShippingPage = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping Details</h1>
       <Form onSubmit={addShipping}>
         <Form.Group controlId="address">
