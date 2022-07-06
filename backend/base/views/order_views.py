0d179d430797162f4bb3e1db4413d784d2b88e55
Fransiskus <rochesterfransiskusaldo@gmail.com>
2022-07-06 13:28:02 -0400

    create order view

---
+++ backend/base/views/order_views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from rest_framework import status
+from backend.base.models import OrderItem, ShippingAddress, Order, Product
+
+
+@api_view(["POST"])
+@permission_classes(["IsAuthenticated"])
+def addOrderItems(request):
+
+  user = request.user 
+  data = request.data
+  orderItems = data['orderItems']
+
+  if orderItems and len(orderItems) == 0:
+    return Response({"detail": "No Order Items"}, status=status.HTTP_400_BAD_REQUEST)
+  else:
+    #create order
+    order = Order.objects.create(
+      user=user,
+      paymentMethod=data["paymentMethod"],
+      taxPrice=data["taxPrice"],
+      shippingPrice=data["shippingPrice"],
+      totalPrice=data["totalPrice"]
+    )
+  
+    #create shipping address
+    shipping = ShippingAddress.objects.create(
+      
+      order=order,
+      address=data["shippingAddress"]["address"],
+      city=data["shippingAddress"]["city"],
+      postalCode=data["shippingAddress"]["postalCode"],
+      province=data["shippingAddress"]["province"],
+      country=data["shippingAddress"]["country"],
+      phoneNumber=data["shippingAddress"]["phoneNumber"]
+      
+    )
+    #create order and set order to orderItem relationship
+    for i in orderItems:
+      product = Product.objects.get(id=i["product"])
+      item = OrderItem.objects.create(
+        product=product,
+        order=order,
+        name=product.name,
+        quantity=i["price"],
+        price=i["price"],
+        image=product.image.url
+      )
+
+    #update stock
+
+    product.stock -= item.quantity
+    product.save()
+    
+  return Response("ORDER")
